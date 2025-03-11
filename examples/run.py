986627c0954e0f8e1bace3284e9edf6353db90f6
Shubham Gupta <11032835+shubham3121@users.noreply.github.com>
2025-03-11 19:00:07 +0530

    start with custom run simulations - add an quickstart example

---
+++ examples/run.py
@@ -0,0 +1,26 @@
+from quickstart.quickstart.server_app import app as server_app
+from quickstart.quickstart.client_app import app as client_app
+from flwr.simulation import run_simulation
+from syft_flwr.server import run_simulation as custom_run_simulation
+
+
+def run():
+    NUM_CLIENTS = 3
+    backend_config = {"client_resources": {"num_cpus": 1, "num_gpus": 0.0}}
+
+    # run_simulation(
+    #     server_app=server_app,
+    #     client_app=client_app,
+    #     num_supernodes=NUM_CLIENTS,
+    #     backend_config=backend_config,
+    # )
+    custom_run_simulation(
+        server_app=server_app,
+        client_app=client_app,
+        num_supernodes=NUM_CLIENTS,
+        backend_config=backend_config,
+    )
+
+
+if __name__ == "__main__":
+    run()
