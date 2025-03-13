8b4811817067a5113f3299c7ae1572e209ab0480
rasswanth-s <43314053+rasswanth-s@users.noreply.github.com>
2025-03-13 15:33:29 +0530

    push update to client app

--- src/syft_flwr/client_syft_event.py
+++ src/syft_flwr/client_syft_event.py
@@ -9,7 +9,14 @@ box = SyftEvents("flwr")
 
 @box.on_request("/messages")
 def handle_messages(request: Request) -> None:
-    logger.info(f"Received request: {request.body}")
+    logger.info(f"Received request size: {len(request.body)} bytes")
+    # proto = request_to_proto(request.body)
+    # message = proto_to_message(proto)
+
+
+    # reply_message = client_app(message=message, context=context)
+
+    
 
 
 if __name__ == "__main__":
