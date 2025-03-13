2f7ad6650276a9678ef0e472058004fb093455c2
Khoa Duy Nguyen <88959106+khoaguin@users.noreply.github.com>
2025-03-13 17:12:17 +0700

    deserializing request message

--- src/syft_flwr/client_syft_event.py
+++ src/syft_flwr/client_syft_event.py
@@ -3,6 +3,8 @@ from __future__ import annotations
 from loguru import logger
 from syft_event import SyftEvents
 from syft_event.types import Request
+from flwr.common.serde import message_from_proto
+from flwr.proto.messages_pb2 import Message as ProtoMessage
 
 box = SyftEvents("flwr")
 
@@ -10,13 +12,10 @@ box = SyftEvents("flwr")
 @box.on_request("/messages")
 def handle_messages(request: Request) -> None:
     logger.info(f"Received request size: {len(request.body)} bytes")
-    # proto = request_to_proto(request.body)
-    # message = proto_to_message(proto)
-
-
+    message = message_from_proto(ProtoMessage.FromString(request.body))
+    logger.info(f"Message: {message}")
     # reply_message = client_app(message=message, context=context)
 
-    
 
 
 if __name__ == "__main__":
