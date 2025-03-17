485862bccb0edf0a8f6767b7a5c4f65743895c2c
Yash Gorana <yash.gorana@hotmail.com>
2025-03-17 11:27:55 +0530

    cleanup

--- examples/quickstart/run.py
+++ examples/quickstart/run.py
@@ -1,14 +1,13 @@
+from flwr.simulation import run_simulation
 from quickstart.quickstart.client_app import app as client_app
 from quickstart.quickstart.server_app import app as server_app
-
-from syft_flwr.server import run_simulation as custom_run_simulation
 
 
 def run():
     NUM_CLIENTS = 3
     backend_config = {"client_resources": {"num_cpus": 1, "num_gpus": 0.0}}
 
-    custom_run_simulation(
+    run_simulation(
         server_app=server_app,
         client_app=client_app,
         num_supernodes=NUM_CLIENTS,
