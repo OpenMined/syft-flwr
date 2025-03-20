b17999df377ccd9310b589fd09d909b9e0e865bb
rasswanth-s <43314053+rasswanth-s@users.noreply.github.com>
2025-03-20 16:26:14 +0530

    add syft flower run server to run.py

--- src/syft_flwr/run.py
+++ src/syft_flwr/run.py
@@ -1,15 +1,16 @@
 from uuid import uuid4
 
 from flwr.client.client_app import LoadClientAppError
+from flwr.server.server_app import LoadServerAppError
 from flwr.common import Context
 from flwr.common.object_ref import load_app
-from flwr.common.typing import UserConfig
 from flwr.common.record import RecordSet
 
 
+from syft_flwr.flower_server import syftbox_flwr_server
 from syft_flwr.config import load_and_validate
 
-__all__ = ["syftbox_run_flwr_client"]
+__all__ = ["syftbox_run_flwr_client", "syftbox_run_flwr_server"]
 
 
 def syftbox_run_flwr_client(flower_project_dir):
@@ -34,11 +35,22 @@ def syftbox_run_flwr_client(flower_project_dir):
     syftbox_flwr_client(client_app, context)
 
 
-def syftbox_run_flwr_client(flower_project_dir):
-    from syft_flwr.flower_server import syftbox_flwr_server
+def syftbox_run_flwr_server(flower_project_dir):
     
     pyproject_conf = load_and_validate(flower_project_dir)
     server_ref = pyproject_conf["tool"]["flwr"]["app"]["components"]["serverapp"]
 
-    # todo rest
-    syftbox_flwr_server(...)
+    context = Context(
+        run_id=uuid4().int,
+        node_id=uuid4().int,
+        node_config={},
+        state=RecordSet(),
+        run_config={}, 
+    )
+    server_app = load_app(
+        server_ref,
+        LoadServerAppError,
+        flower_project_dir,
+    )
+
+    syftbox_flwr_server(server_app, context)
