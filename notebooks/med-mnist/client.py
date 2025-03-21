afed8afdb47addd74174e25ac80e60b595416758
Khoa Duy Nguyen <88959106+khoaguin@users.noreply.github.com>
2025-03-21 11:38:13 +0700

    run server and client with python files

---
+++ notebooks/med-mnist/client.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+from syft_flwr.run import syftbox_run_flwr_client
+
+# DATA_DIR = os.environ["DATA_DIR"]
+# OUTPUT_DIR = os.environ["OUTPUT_DIR"]
+
+# TODO - how does DATA_DIR pass information down
+# TODD - how does OUTPUT_DIR work here. As data is constantly passed around over RPC
+# This means we need to mount rpc directory into the vm for active communication
+syftbox_run_flwr_client(Path(__file__).parent / "pyproject.toml")
