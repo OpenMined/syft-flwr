fc8da6fa83dd9ff4c28928a51f68eee652068389
Khoa Duy Nguyen <88959106+khoaguin@users.noreply.github.com>
2025-03-24 11:14:42 +0700

    syft_flwr.bootstrap

--- src/syft_flwr/main.py
+++ src/syft_flwr/main.py
@@ -1,11 +1,16 @@
+from pathlib import Path
+
 import typer
+from rich import print as rprint
+from typing_extensions import Annotated
 
 from syft_flwr import __version__
+from syft_flwr.utils import _copy_main_py, _update_pyproject_toml
 
 app = typer.Typer(
-    name="syft-flwr",
+    name="syft_flwr",
     help=typer.style(
-        "syft-flwr CLI",
+        "Welcome to the command line interface for syft_flwr",
         fg=typer.colors.BRIGHT_YELLOW,
         bold=True,
     ),
@@ -17,8 +22,32 @@ app = typer.Typer(
 
 @app.command(rich_help_panel="General Options")
 def version() -> None:
-    """Print version"""
+    """Print syft-flwr version"""
     print(f"Welcome to syft_flwr version {__version__}")
+
+
+@app.command(rich_help_panel="Setup Commands")
+def bootstrap(
+    path: Annotated[
+        str, typer.Argument(help="Absolute path to the flwr project to bootstrap")
+    ],
+) -> None:
+    """Bootstrap a new syft-flwr project from the flwr project at the given path"""
+    flwr_project_dir = Path(path).expanduser().resolve()
+
+    if not flwr_project_dir.exists():
+        rprint(f"[red]The flower project '{flwr_project_dir}' does not exist[/red]")
+        raise typer.Exit(code=1)
+
+    if not (flwr_project_dir / "pyproject.toml").exists():
+        rprint(
+            f"[red]There is no `pyproject.toml` in the flower project '{flwr_project_dir}'[/red]"
+        )
+        raise typer.Exit(code=1)
+
+    rprint(f"[green]Bootstrapping syft-flwr project from '{flwr_project_dir}'[/green]")
+    _copy_main_py(flwr_project_dir)
+    _update_pyproject_toml(flwr_project_dir)
 
 
 def main() -> None:
--- src/syft_flwr/utils.py
+++ src/syft_flwr/utils.py
@@ -1,10 +1,12 @@
 import hashlib
 from pathlib import Path
+from typing import Union
 
-import tomllib  # TODO: replace with tomli, as it is not supported older python versions.
-from flwr.client.client_app import LoadClientAppError
-from flwr.common.object_ref import load_app
-from flwr.server.server_app import LoadServerAppError
+import toml
+import typer
+from rich import print as rprint
+
+from syft_flwr.config import load_config
 
 
 def string_to_hash_int(input_string: str) -> int:
@@ -15,32 +17,70 @@ def string_to_hash_int(input_string: str) -> int:
     return hash_int
 
 
-def read_toml_file(file_path):
-    """Read a TOML file and return the data."""
-    file_path = to_path(file_path)
-    with open(file_path, "rb") as file:
-        data = tomllib.load(file)
-    return data
+def _copy_main_py(flwr_project_dir: Path) -> None:
+    """Copy the content below to `main.py` file to the syft-flwr project"""
+    main_py_path = flwr_project_dir / "main.py"
+    if main_py_path.exists():
+        rprint(
+            f"[red]The `main.py` file already exists in the syft-flwr project '{flwr_project_dir}'[/red]"
+        )
+        raise typer.Exit(code=1)
+    main_py_content = """
+import os
+from pathlib import Path
+
+from syft_core import Client
+
+from syft_flwr.config import load_config
+from syft_flwr.run import syftbox_run_flwr_client, syftbox_run_flwr_server
+
+DATA_DIR = os.environ["DATA_DIR"]
+OUTPUT_DIR = os.environ["OUTPUT_DIR"]
 
 
-def to_path(path: str) -> Path:
-    return Path(path).expanduser().resolve()
+flower_project_dir = Path(__file__).parent.absolute()
+client = Client.load()
+config = load_config(flower_project_dir)
+
+is_client = client.email in config["tool"]["syft_flwr"]["datasites"]
+is_server = client.email in config["tool"]["syft_flwr"]["aggregator"]
+
+if is_client:
+    # run by each DO
+    syftbox_run_flwr_client(flower_project_dir)
+elif is_server:
+    # run by the DS
+    syftbox_run_flwr_server(flower_project_dir)
+else:
+    raise ValueError(f"{client.email} is not in config.datasites or config.aggregator")
+    """
+    with open(main_py_path, "w") as f:
+        f.write(main_py_content)
 
 
-def load_server_app(flower_conf, flower_project_dir):
-    """Load the Flower server app."""
+def _update_pyproject_toml(flwr_project_dir: Union[str, Path]) -> None:
+    """Update the `pyproject.toml` file to the syft-flwr project"""
+    flwr_project_dir = Path(flwr_project_dir)
+    pyproject_conf = load_config(flwr_project_dir, check_module=False)
 
-    return load_app(
-        flower_conf["tool"]["flwr"]["app"]["components"]["serverapp"],
-        LoadServerAppError,
-        flower_project_dir,
-    )
+    # TODO: remove this after we find out how to pass the right context to the clients
+    pyproject_conf["tool"]["flwr"]["app"]["config"]["partition-id"] = 0
+    pyproject_conf["tool"]["flwr"]["app"]["config"]["num-partitions"] = 1
 
+    # Remove any existing syft_flwr section if exists
+    if "syft_flwr" in pyproject_conf.get("tool", {}):
+        del pyproject_conf["tool"]["syft_flwr"]
 
-def load_client_app(flower_conf, flower_project_dir):
-    """Load the Flower client app."""
-    return load_app(
-        flower_conf["tool"]["flwr"]["app"]["components"]["clientapp"],
-        LoadClientAppError,
-        flower_project_dir,
+    with open(flwr_project_dir / "pyproject.toml", "w") as f:
+        toml_str = toml.dumps(pyproject_conf)
+        f.write(toml_str)
+
+    # Append the syft_flwr section at the end of the file
+    with open(flwr_project_dir / "pyproject.toml", "a") as f:
+        f.write("\n[tool.syft_flwr]\n")
+        f.write('datasites = ["b@openmined.org", "c@openmined.org"]\n')
+        f.write('aggregator = "a@openmined.org"\n')
+
+    rprint(
+        "[green]Updated pyproject.toml with syft_flwr template configuration. Please check and change according to your needs.[/green]"
     )
