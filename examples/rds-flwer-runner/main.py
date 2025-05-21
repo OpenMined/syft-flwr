4aff15d7376c3823fe26b2eb5c89418d129621ff
Yash Gorana <yash.gorana@hotmail.com>
2025-05-21 13:04:25 +0530

    add flower runner app

---
+++ examples/rds-flwer-runner/main.py
@@ -0,0 +1,29 @@
+from time import sleep
+
+import syft_rds as sy
+from syft_core import Client
+from syft_rds.models.models import JobStatus
+
+email = Client.load().email
+print(f"SyftBox client email: {email}")
+client = sy.init_session(email)
+
+
+def main():
+    print("Waiting for jobs...")
+    while True:
+        jobs = client.jobs.get_all(status="pending_code_review")
+        for job in jobs:
+            if job.status == JobStatus.pending_code_review:
+                print("Got job", job.uid)
+                print(job.user_code)
+                print("Job started", job.uid)
+                res_job = client.run_private(job)
+                print(res_job)
+                print("Job finished", job.uid)
+
+        sleep(10)
+
+
+if __name__ == "__main__":
+    main()
